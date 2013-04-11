7cda79c62509770549a076dc31e87305fd08e999
Brian Martin <brianmartin@gmail.com>
2013-04-10 21:31:48 -0400

    more stuff

--- leader-pigs/src/main/scala/com/github/harshal/distos/LeaderPigs.scala
+++ leader-pigs/src/main/scala/com/github/harshal/distos/LeaderPigs.scala
@@ -37,6 +37,10 @@ object Util {
     log.level = Level.DEBUG
     log.console.enabled = true
   }
+  
+  // Clock Utils
+  var NETWORK_DELAY: Int = 5
+  def simulateNetworkDelay(clock: Clock): Unit = for (_ <- 0 to rand.nextInt(NETWORK_DELAY)) clock.tick()
 }
 
 //
@@ -99,8 +103,24 @@ trait Neighbors extends AbstractPig with Logging {
       port -> select(Node("localhost", port), Symbol(port.toString))
     ).toSeq)
   }
+  
+  def flood(msg: Any) = for (n <- neighbors.values) n ! msg
 
 }
+
+//
+// Lamport Clock
+//
+case class Clock {
+  @volatile var _clockValue: Int = 0
+  def tick(): Clock = { _clockValue += 1; this }
+  def setMax(that: Clock) = _clockValue = Math.max(this._clockValue, that._clockValue)
+  def copy() = { val c = new Clock; c._clockValue = _clockValue; c }
+}
+
+trait LamportClock {
+  val clock: Clock = new Clock
+} 
 
 //
 // Leader Election
@@ -132,6 +152,8 @@ trait RingBasedLeaderElection extends AbstractPig {
   import Constants.ELECTION_TIMEOUT
   
   var leader: Option[AbstractActor] = None
+  
+  def amLeader: Boolean = leader == Some(this)
 
   override def actions = super.actions ++ Seq(action)
     
@@ -187,10 +209,35 @@ case class StoneColumn(position: Int)
 
 // TODO
 object GameMessages {
-  case class SomeMessage()
+  
+  // Game Engine => ()
+  case class Start()
+
+  // Game Engine => Pig
+  case class Map(map: Seq[Option[Int]])
+  case class Trajectory(x: Int)
+  case class Status()
+  case class EndGame()
+  case class GetPosition()
+  case class GetPort()
+  case class Port(x: Int)
+  case class Position(x: Int)
+  case class SetPosition(x: Int)
+  case class SetLeft(x: Int)
+  case class SetRight(x: Int)
+  
+  // Pig => Game Engine
+  case class Done()
+  case class WasHit(status: Boolean)
+  
+  type GameMap = Seq[Option[Int]]
+  
+  // Pig => Pig
+  case class BirdApproaching(position: Int, clock: Clock)
+  
 }
 trait PigGameLogic extends AbstractPig {
-  this: AbstractPig with Neighbors with RingBasedLeaderElection =>
+  this: AbstractPig with Neighbors with RingBasedLeaderElection with LamportClock =>
     
   import GameMessages._
   override def actions = super.actions ++ Seq(action)
@@ -199,6 +246,133 @@ trait PigGameLogic extends AbstractPig {
   // this.leader: Option[AbstractActor]  -- the current leader
   // this.port  : Int -- our port
   // this.neighbors: Map[Int, AbstractActor] -- a map from ports to other pigs
+  
+  var currentPos:Int = -1
+  var moveTime: Option[Clock] = None
+  var hitTime: Option[Clock] = None
+  var gameMap: GameMap = null
+  
+  def available(pos: Int): Boolean = {
+    if (validPos(pos)){
+      gameMap(pos) match {
+        case None => true
+        case _ => false
+      }
+    }
+    else false
+  }
+  
+  def move():Boolean =  {
+    if (available(currentPos - 1)){
+      currentPos -= 1
+      true
+    }
+    else if (available(currentPos + 1)){
+      currentPos += 1
+      true
+    }
+    else
+      false
+  }
+
+  def move(pos:Int):Boolean={
+    if (available(pos)) {
+      currentPos = pos
+      true
+    }
+    else false
+  }
+
+  def moveIfRequired(targetPos: Int) = {
+    if (isMoveRequired(targetPos))
+      !move(currentPos+1) //if unable to move return true
+    else
+      false //if safe return false
+  }
+
+  def isMoveRequired(targetPos: Int) =
+    (currentPos-1 == targetPos) || 
+    (currentPos-2 == targetPos && isColumn(currentPos-1))
+
+  def isColumn(pos: Int)  = if (validPos(pos)) gameMap(pos) == Some(COLUMN) else false
+  def isNotEmpty(pos:Int) = if (validPos(pos)) gameMap(pos) != None else false
+
+  def checkIfHit(targetPos: Int) = !!! // Should check the clock here
+        //    (targetPos == currentPos) || 
+        //    (targetPos == currentPos - 1 && isNotEmpty(targetPos)) ||
+        //    (targetPos == currentPos - 2 && isNotEmpty(targetPos) && isColumn(currentPos - 1))
+
+  def validPos(pos:Int) = !(pos < 0 || gameMap.size <= pos)
+  
+  private val action = {
+    case Map(map) => { 
+      this.gameMap = map
+      sender ! Ack
+    }
+    case Trajectory(targetPos) => {
+      if (amLeader)
+        flood(BirdApproaching(targetPos, clock.tick()))
+    }
+    case BirdApproaching(targetPos, incomingClock) => { 
+      
+      clock.setMax(incomingClock)
+      Util.simulateNetworkDelay(clock)
+      
+      // deal with ourselves
+      if (targetPos == currentPos - 1 && isColumn(currentPos-1)) {
+        // TODO
+        val success = move()
+        clock.tick()
+        if (!success) moveTime = Some(clock.copy())
+      }
+      // TODO: 
+//      else if (currentPos == targetPos){
+//        val success = move()
+//        if (!success){
+//          left ! TakeShelter(targetPos,gameMap.size)
+//          right ! TakeShelter(targetPos,gameMap.size)
+//          hit = true
+//        }
+//      }
+//      else if (currentPos == targetPos && gameOver) {
+//        hit = true
+//      }
+    }
+    // TODO: 
+//    case TakeShelter(targetPos,hopCount) => {
+//      if (hopCount > 0){
+//        left  ! TakeShelter(targetPos, hopCount - 1)
+//        right ! TakeShelter(targetPos, hopCount - 1)
+//      }
+//      if (targetPos!=currentPos && !gameOver){
+//        hit = moveIfRequired(targetPos)
+//      }
+//      else if (targetPos!=currentPos && isMoveRequired(targetPos)) //game over but not yet updated state
+//        hit = true
+//    }
+//    case Status() => {
+//      sender ! WasHit(hit)
+//    }
+//    case GetPort() => { sender ! Port(port) }
+//    case GetPosition() => { sender ! Position(currentPos) }
+//    case SetPosition(x) => {
+//      currentPos = x
+//      hit = false
+//      sender ! Done
+//    }
+//    case SetLeft(port) => {
+//      left = select(Node("localhost", port), Symbol(port.toString))
+//      sender ! Done()
+//    }
+//    case SetRight(port) => {
+//      right = select(Node("localhost", port), Symbol(port.toString))
+//      sender ! Done()
+//    }
+//    case Exit => {  sender ! Done(); exit() }
+//    case m => throw new Error("Unknown message: " + m)
+  }
+}
+
   
   private val action: Any ~> Unit  = {
     case SomeMessage => !!!
@@ -210,7 +384,7 @@ trait PigGameLogic extends AbstractPig {
 // Running it all.
 //
 
-class Pig(val port: Int) extends AbstractPig with Neighbors with RingBasedLeaderElection
+class Pig(val port: Int) extends AbstractPig with Neighbors with RingBasedLeaderElection with LamportClock
 
 object Main extends App {
   
